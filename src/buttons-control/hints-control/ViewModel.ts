2f7ef619f537204ef95dd0048c0059266891a55d
whatever <voronov.xcvi@gmail.com>
2023-12-07 13:05:07 +0300

    ui state inconsistency fix

--- src/buttons-control/hints-control/ViewModel.ts
+++ src/buttons-control/hints-control/ViewModel.ts
@@ -1,16 +1,21 @@
-import { autorun, flowResult, makeAutoObservable, reaction } from "mobx";
-import { CountryInfo, getCountryByName } from "../../api/apiService";
 import { debounce } from "lodash";
+import { flowResult, makeAutoObservable } from "mobx";
+import { CountryInfo, getCountryByName } from "../../api/apiService";
+
+interface TimestampedCountryInfo {
+  res: CountryInfo[];
+  timestamp: number;
+}
 
 class ThirdBlockViewModel {
   inputValue = "";
   maxHints = 3;
-  hints: CountryInfo[] = [];
+  hints: TimestampedCountryInfo = {
+    res: [],
+    timestamp: Date.now(),
+  };
   state: "done" | "pending" | "error" = "done";
-  // finishedEditing = true;
-  // lastEdit: number = Date.now();
-  private maxDelay: number = 1000;
-  // fieldDisabled = false;
+  private maxDelay: number = 500;
   private debouncedGetHints = debounce((value) => this.getHints(value), this.maxDelay);
 
   constructor(private readonly getCountryByName: (value: string) => Promise<CountryInfo[]>) {
@@ -19,26 +24,20 @@ class ThirdBlockViewModel {
 
   onChange = (value: string) => {
     this.inputValue = value;
-    // const now = Date.now();
-    // const diff = now - this.lastEdit;
-    // this.finishedEditing = false;
-    // if (diff > this.maxDelay) {
-    //   console.log(diff, "will fire");
-    // } else {
-    //   console.log(diff, "will not fire");
-    // }
-    // this.lastEdit = Date.now();
-    // setTimeout(() => this.getHints(value), 1000);
+
     this.debouncedGetHints(value);
   };
 
-  getHints = function* <T extends CountryInfo = CountryInfo>(this: ThirdBlockViewModel, value: string): Generator<T[]> {
+  getHints = function* (this: ThirdBlockViewModel, value: string): Generator<TimestampedCountryInfo> {
+    const timestamp = Date.now();
     this.state = "pending";
     console.log("hints");
     try {
       const countries = yield flowResult(this.getCountryByName(value));
+      if (this.hints.timestamp < timestamp) {
+        this.hints = { res: countries as CountryInfo[], timestamp };
+      }
       this.state = "done";
-      this.hints = countries as T[];
     } catch (e) {
       this.state = "error";
     }
@@ -50,54 +49,10 @@ class ThirdBlockViewModel {
   };
 
   get limitedHints() {
-    return this.hints.slice(0, this.maxHints);
+    return this.hints.res.slice(0, this.maxHints);
   }
 }
 
 const viewModel = new ThirdBlockViewModel(getCountryByName);
-
-// reaction(
-//   () => viewModel.inputValue,
-//   (inputValue) => {
-
-//     console.log("finishedEditing ", "false");
-//   }
-// );
-
-// const disposer =
-// reaction(
-//   () => viewModel.finishedEditing,
-//   (finishedEditing) => {
-//     if (!finishedEditing) {
-//       setTimeout(() => {
-//         finishedEditing = true;
-//         console.log("finishedEditing ", "true");
-//       }, 1000);
-//     }
-//   }
-// );
-
-// disposer()
-
-// reaction(
-//   () => viewModel.inputValue,
-//   (inputValue) => {
-//     if (viewModel.finishedEditing) {
-//       viewModel.finishedEditing = false;
-//       console.log("finishedEditing ", "false");
-//       setTimeout(() => {
-//         viewModel.finishedEditing = true;
-//         console.log("finishedEditing ", "true");
-//       }, 1000);
-//     }
-//   }
-// );
-
-// autorun(() => {
-//   if (viewModel.inputValue && viewModel.finishedEditing) {
-//     console.log("autorun " + viewModel.inputValue);
-//     viewModel.getHints(viewModel.inputValue);
-//   }
-// });
 
 export default viewModel;
